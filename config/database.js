a82164c63037da86b96c9496e9fb0d289f554655
carloV <cviana66@gmail.com>
2021-01-17 17:00:58 +0100

    pulizia

--- config/database.js
+++ config/database.js
@@ -1,3 +1,7 @@
 module.exports = {
-  'url' : 'mongodb://sb:sharing11beer@ds045464.mongolab.com:45464/dbm1'
+ // 'url' : 'mongodb://sb:sharing11beer@ds045464.mongolab.com:45464/dbm1'
+ //'url' : 'mongodb://sb:sharing11beer@dbm1-shard-00-00.pohw0.mongodb.net:27017,dbm1-shard-00-01.pohw0.mongodb.net:27017,dbm1-shard-00-02.pohw0.mongodb.net:27017/dbm1?ssl=true&replicaSet=atlas-67aqrm-shard-0&authSource=admin&retryWrites=true&w=majority'
+'url' : 'mongodb+srv://sb:sharing11beer@dbm1.pohw0.mongodb.net/dbm1?retryWrites=true&w=majority'
 }
+
+// mongo ds045464.mlab.com:45464/dbm1 -u sb -p sharing11beer
