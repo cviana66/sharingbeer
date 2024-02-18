e25ca0b8a892133eb9d21d8bbd596fbac3706946
carloV <cviana66@gmail.com>
2024-02-18 22:23:49 +0100

    update 18.02.24 - axerve originale da afag

--- app/routesAxerve.js
+++ app/routesAxerve.js
@@ -15,11 +15,63 @@ var users;
 
   });
 
+//GET
+  app.get('/axerve', function(req, res) {
+
+    // Build the post string from an object
+    var post_data = JSON.stringify({
+      'shopLogin' : 'GESPAY63388',
+      'amount' : '1',
+      'currency' : 'EUR',
+      'shopTransactionID' : 'TEST-ONE'
+    });
+
+    // Setup the post call headers
+    var axerveHeaders = {
+      "Content-Type": "application/json",  
+      "Authorization": "apikey R0VTUEFZNjMzODgjI0VzZXJjZW50ZSBUZXN0IGRpIFZJQU5BIyMyNS8xMC8yMDIzIDExOjI1OjAy"
+    };
+
+    // An object of options to indicate where to post to
+    var axerveOptions = {
+      host: "sandbox.gestpay.net",
+      path: '/api/v1/payment/create',
+      method: 'POST',
+      headers: axerveHeaders
+    };
+    console.log('Preparo la chiamata: ', axerveOptions);
+
+    // Set up the request
+    var post_req =  https.request(axerveOptions, function(resPost) {
+        //resPost.setEncoding('utf8');
+        //console.log("POST_REQ= ",post_req);
+        resPost.on('data', function (d) {
+            //console.log('Response... ', d);
+
+            dParsed = JSON.parse(d);
+            console.log('dParsed dentro', dParsed.payload);
+
+            res.render('axerveDE.njk', {'paymentID':dParsed.payload.paymentID, 'paymentToken': dParsed.payload.paymentToken});
+
+        });
+
+        res.on('error', function (e) {
+            console.log('ERRORE !!! ', e);
+        });
+
+    });
+
+    // post the data
+    console.log("Fuori")
+    post_req.write(post_data);
+    post_req.end();
+
+
+  });
 
 //POST
   app.post('/axerve_new', function(req, res) {
-    console.log("REQ:BODY",req.body)
-    console.log("APIKEY",process.env.APIKEY)
+
     var amount = req.body.amountEur;
     var transactionID = req.body.transactionID;
     var error = req.body.error;
@@ -73,9 +125,7 @@ var users;
 
       } else {
         dParsed = data;
-        res.render('axerveDE.njk', {'shopLogin':shopLogin, 
-                                    'paymentID':dParsed.payload.paymentID, 
-                                    'paymentToken': dParsed.payload.paymentToken});
+        res.render('axerveDE.njk', {'shopLogin':shopLogin, 'paymentID':dParsed.payload.paymentID, 'paymentToken': dParsed.payload.paymentToken});
 
       }
     })
@@ -84,58 +134,3 @@ var users;
   });
 
 };
-
-/*/GET
-  app.get('/axerve', function(req, res) {
-
-    // Build the post string from an object
-    var post_data = JSON.stringify({
-      'shopLogin' : 'GESPAY63388',
-      'amount' : '1',
-      'currency' : 'EUR',
-      'shopTransactionID' : 'TEST-ONE'
-    });
-
-    // Setup the post call headers
-    var axerveHeaders = {
-      "Content-Type": "application/json",  
-      "Authorization": process.env.APIKEY
-    };
-
-    // An object of options to indicate where to post to
-    var axerveOptions = {
-      host: "sandbox.gestpay.net",
-      path: '/api/v1/payment/create',
-      method: 'POST',
-      headers: axerveHeaders
-    };
-    console.log('Preparo la chiamata: ', axerveOptions);
-
-    // Set up the request
-    var post_req =  https.request(axerveOptions, function(resPost) {
-        //resPost.setEncoding('utf8');
-        //console.log("POST_REQ= ",post_req);
-        resPost.on('data', function (d) {
-            //console.log('Response... ', d);
-
-            dParsed = JSON.parse(d);
-            console.log('dParsed dentro', dParsed.payload);
-
-            res.render('axerveDE.njk', {'paymentID':dParsed.payload.paymentID, 'paymentToken': dParsed.payload.paymentToken});
-
-        });
-
-        res.on('error', function (e) {
-            console.log('ERRORE !!! ', e);
-        });
-
-    });
-
-    // post the data
-    console.log("Fuori")
-    post_req.write(post_data);
-    post_req.end();
-
-    
-  });
-*/
