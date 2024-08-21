e938e465296cde80c399a6c58d64737ec8a30cb8
carloV <cviana66@gmail.com>
2024-08-21 19:13:40 +0200

    update 210824 - gestione poup

--- public/js/popups.js
+++ public/js/popups.js
@@ -1,6 +1,7 @@
 /* POPUPs Display */
 
 function openDocument (htmlFile,focusElement) {
+  console.log(htmlFile,focusElement)
   var blur = document.getElementsByClassName('blur');
   for (var i = 0; i < blur.length; i++) {
       blur[i].classList.toggle('active');
@@ -15,9 +16,7 @@ function openDocument (htmlFile,focusElement) {
   const myRequest = new Request(htmlFile, init);
   const container = document.getElementById("popup");
 
-  if (focusElement=='infoInfo') {
-    console.log('infoInfo');
-
+  if (focusElement=='infoPrivacy'){    
     fetch(htmlFile, init)
       .then((response) => {
         if (!response.ok) {
@@ -25,9 +24,9 @@ function openDocument (htmlFile,focusElement) {
         }
         return response.text();
       })
-      .then((data) => {
+      .then((data) => {        
         var bX =  '<div class="sticky"> \
-                    <a href="#" onclick="return closeDocumentInfo()"> \
+                    <a href="#" onclick="return closeDocument(\'checkPrivacy\')"> \
                       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg> \
                     </a> \
                   </div>'
@@ -35,15 +34,9 @@ function openDocument (htmlFile,focusElement) {
         container.innerHTML = bX + data;
         // Scorri automaticamente alla parte superiore
         const mainElement = document.querySelector('div.top'); 
-          if (mainElement) {
-            //console.log('Elemento principale', mainElement);
-            mainElement.scrollIntoView({ behavior: 'instant', block: 'start' });
-          } else {
-            //console.error('Elemento principale non trovato');
-          }
+          if (mainElement) mainElement.scrollIntoView({ behavior: 'instant', block: 'start' });          
       });
   } else {
-    console.log('infoPrivacy');
     fetch(htmlFile, init)
       .then((response) => {
         if (!response.ok) {
@@ -53,7 +46,7 @@ function openDocument (htmlFile,focusElement) {
       })
       .then((data) => {
         var bX =  '<div class="sticky"> \
-                    <a href="#" onclick="return closeDocumentPrivacy()"> \
+                    <a href="#" onclick="return closeDocument()"> \
                       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg> \
                     </a> \
                   </div>'
@@ -61,39 +54,31 @@ function openDocument (htmlFile,focusElement) {
         container.innerHTML = bX + data;
         // Scorri automaticamente alla parte superiore
         const mainElement = document.querySelector('div.top'); 
-          if (mainElement) {
-            //console.log('Elemento principale', mainElement);
-            mainElement.scrollIntoView({ behavior: 'instant', block: 'start' });
-          } else {
-            //console.error('Elemento principale non trovato');
-          }
+          if (mainElement) { mainElement.scrollIntoView({ behavior: 'instant', block: 'start' }); }
       });
-  } 
+  }
+  
   container.style.display = "block";
   container.focus();
 }
 
-function closeDocumentPrivacy () {
+async function closeDocument(focusElement) {
+  console.log('FOCUS',focusElement)
   document.getElementById("popup").style.display = "none";
   
   var blur = document.getElementsByClassName('blur');
   for (var i = 0; i < blur.length; i++) {
       blur[i].classList.toggle('active');
   }
-  setTimeout(function() { // Aspetta un po' prima di applicare il focus
-          var element = document.getElementById('checkPrivacy');          
-              element.focus();
-      }, 100);
-}
-function closeDocumentInfo () {
-  document.getElementById("popup").style.display = "none";
-  
-  var blur = document.getElementsByClassName('blur');
-  for (var i = 0; i < blur.length; i++) {
-      blur[i].classList.toggle('active');
+  if (focusElement != null) {
+    await sleep(200)
+    var element = document.getElementById(focusElement);          
+    element.focus();
   }
-  setTimeout(function() { // Aspetta un po' prima di applicare il focus
-          var element = document.getElementById('infoInfo');          
-              element.focus();
-      }, 100);
 }
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    })
+  }
